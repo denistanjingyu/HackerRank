fc19531bbd799f87aee9b17b553468093a7cee03
Tan Jing Yu, Denis <denis.tanjingyu@Hotmail.sg>
2020-08-12 02:25:46 +0800

    Add files via upload

---
+++ Weather Observation Station 20.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+
+SELECT ROUND(s.LAT_N, 4) 
+FROM STATION s 
+WHERE (SELECT ROUND(COUNT(s.ID) / 2) - 1 
+       FROM STATION) 
+       = 
+      (SELECT COUNT(s1.ID) 
+       FROM STATION s1 
+       WHERE s1.LAT_N < s.LAT_N);
