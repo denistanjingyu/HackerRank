fed058087d2d9d9e445652578e06c837bcdbb777
Tan Jing Yu, Denis 陈劲宇 <denis.tanjingyu@Hotmail.sg>
2020-09-07 04:25:13 +0800

    Update Type of Triangle.sql
    Spacing

--- Type of Triangle.sql
+++ Type of Triangle.sql
@@ -4,11 +4,11 @@ Enter your query here.
 
 SELECT 
 CASE 
-	WHEN A + B > C AND A + C > B AND B + C > A THEN 
+    WHEN A + B > C AND A + C > B AND B + C > A THEN 
     CASE 
-		WHEN A = B AND B = C THEN 'Equilateral' 
-		WHEN A = B OR B = C OR A = C THEN 'Isosceles' 
+	WHEN A = B AND B = C THEN 'Equilateral' 
+	WHEN A = B OR B = C OR A = C THEN 'Isosceles' 
         WHEN A != B OR B != C OR A != C THEN 'Scalene' 
-	END ELSE 'Not A Triangle' 
+    END ELSE 'Not A Triangle' 
 END 
 FROM TRIANGLES;
