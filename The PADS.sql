1840b0e8649e8922b27e9e61574de72105b3db63
Tan Jing Yu, Denis 陈劲宇 <denis.tanjingyu@Hotmail.sg>
2020-11-28 14:16:04 +0800

    Add files via upload

---
+++ The PADS.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+*/
+
+SELECT CONCAT(Name, '(', LEFT(Occupation, 1), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation),
+         Occupation;
