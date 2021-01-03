ac9d1679005af9c1ca911cda1ad9f550fe6f075f
Denis <denis.tanjingyu@hotmail.sg>
2021-01-03 20:16:31 +0800

    add 3 python files

---
+++ Arithmetic Operators.py
@@ -0,0 +1,10 @@
+def arithmetic_operator(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+arithmetic_operator(a, b)
---
+++ Python Division.py
@@ -0,0 +1,9 @@
+def python_division(a, b):
+    print(a // b)
+    print(a / b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+python_division(a, b)
