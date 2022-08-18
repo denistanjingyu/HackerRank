99694d74a2c3ef8441fd43546a70d713ee446396
Sourcery AI <>
2022-08-18 16:28:54 +0000

    'Refactored by Sourcery'

--- Python If-Else.py
+++ Python If-Else.py
@@ -10,12 +10,10 @@ def weird_or_not_weird(n):
     if n % 2 == 1:
         print("Weird")
     elif n % 2 == 0:
-        if n >= 2 and n <= 5:
+        if n >= 2 and n <= 5 or (n < 6 or n > 20) and n > 20:
             print("Not Weird")
-        elif n >= 6 and n <= 20:
+        elif n >= 6:
             print("Weird")
-        elif n > 20:
-            print("Not Weird")
 
 if __name__ == '__main__':
     n = int(input().strip())
--- Write a function.py
+++ Write a function.py
@@ -1,15 +1,8 @@
 def is_leap(year):
-    leap = False
-    
-    # Write your logic here
     if year % 4 == 0:
-        leap = True
-        if year % 100 == 0:
-            leap = False
-            if year % 400 == 0:
-                leap = True
-
-    return leap
+        return year % 400 == 0 if year % 100 == 0 else True
+    else:
+        return False
 
 year = int(input())
 print(is_leap(year))
